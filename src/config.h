a3f9dd1284d316a237e4e93b4a7b59baa143b569
Erinç Uysal <erincuysal@outlook.com>
2024-08-21 00:24:06 +0300

    change keymaps for keychord patch

--- src/config.h
+++ src/config.h
@@ -62,64 +62,64 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
-
+#define TAGKEYS(KEY,TAG)                                                                                               \
+       &((Keychord){1, {{MODKEY, KEY}},                                        view,           {.ui = 1 << TAG} }), \
+       &((Keychord){1, {{MODKEY|ControlMask, KEY}},                            toggleview,     {.ui = 1 << TAG} }), \
+       &((Keychord){1, {{MODKEY|ShiftMask, KEY}},                              tag,            {.ui = 1 << TAG} }), \
+       &((Keychord){1, {{MODKEY|ControlMask|ShiftMask, KEY}},                  toggletag,      {.ui = 1 << TAG} }),
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-m", dmenumon, "-fn", dmenufont,  NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const char vol_up[]      =  "pactl set-sink-volume @DEFAULT_SINK@ +5%; kill -44 $(pidof dwmblocks)" ;
 static const char vol_down[]    =  "pactl set-sink-volume @DEFAULT_SINK@ -5%; kill -44 $(pidof dwmblocks)" ;
 static const char vol_mute[]    =  "pactl set-sink-mute @DEFAULT_SINK@ toggle; kill -44 $(pidof dwmblocks)";
 
-static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	/* { MODKEY,                       XK_Right,  view,           {.ui = ui<< 1} }, */
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-        // Volume control
-    	{ MODKEY,                       XK_KP_Add, spawn,          {.v = vol_up} },
-	{ MODKEY,                       XK_KP_Subtract, spawn,          {.v = vol_down} },
-	{ MODKEY,                       XK_KP_Multiply, spawn,          {.v = vol_mute} },
+static Keychord *keychords[] = {
+   /* Keys        function        argument */
+   &((Keychord){1, {{MODKEY, XK_r}},                                       spawn,          {.v = dmenucmd } }),
+   &((Keychord){1, {{MODKEY, XK_Return}},                        spawn,          {.v = termcmd } }),
+   &((Keychord){2, {{MODKEY, XK_e}, {MODKEY, XK_e}},                       spawn,          {.v = termcmd } }),
+   &((Keychord){1, {{MODKEY, XK_b}},                                       togglebar,      {0} }),
+   &((Keychord){1, {{MODKEY, XK_j}},                                       focusstack,     {.i = +1 } }),
+   &((Keychord){1, {{MODKEY, XK_k}},                                       focusstack,     {.i = -1 } }),
+   &((Keychord){1, {{MODKEY, XK_i}},                                       incnmaster,     {.i = +1 } }),
+   &((Keychord){1, {{MODKEY, XK_d}},                                       incnmaster,     {.i = -1 } }),
+   &((Keychord){1, {{MODKEY, XK_h}},                                       setmfact,       {.f = -0.05} }),
+   &((Keychord){1, {{MODKEY, XK_l}},                                       setmfact,       {.f = +0.05} }),
+   &((Keychord){1, {{MODKEY|ShiftMask, XK_Return}},                                  zoom,           {0} }),
+   &((Keychord){1, {{MODKEY, XK_Tab}},                                     view,           {0} }),
+   &((Keychord){1, {{MODKEY|ShiftMask, XK_c}},                             killclient,     {0} }),
+   &((Keychord){1, {{MODKEY, XK_t}},                                       setlayout,      {.v = &layouts[0]} }),
+   &((Keychord){1, {{MODKEY, XK_f}},                                       setlayout,      {.v = &layouts[1]} }),
+   &((Keychord){1, {{MODKEY, XK_m}},                                       setlayout,      {.v = &layouts[2]} }),
+   &((Keychord){1, {{MODKEY, XK_space}},                                   setlayout,      {0} }),
+   &((Keychord){1, {{MODKEY|ShiftMask, XK_space}},                         togglefloating, {0} }),
+   &((Keychord){1, {{MODKEY, XK_0}},                                       view,           {.ui = ~0 } }),
+   &((Keychord){1, {{MODKEY|ShiftMask, XK_0}},                             tag,            {.ui = ~0 } }),
+   &((Keychord){1, {{MODKEY, XK_comma}},                                   focusmon,       {.i = -1 } }),
+   &((Keychord){1, {{MODKEY, XK_period}},                                  focusmon,       {.i = +1 } }),
+   &((Keychord){1, {{MODKEY|ShiftMask, XK_comma}},                         tagmon,         {.i = -1 } }),
+   &((Keychord){1, {{MODKEY|ShiftMask, XK_period}},                        tagmon,         {.i = +1 } }),
+   &((Keychord){1, {{MODKEY|ShiftMask, XK_q}},                             quit,           {0} }),
+   TAGKEYS(                        XK_1,                      0)
+   TAGKEYS(                        XK_2,                      1)
+   TAGKEYS(                        XK_3,                      2)
+   TAGKEYS(                        XK_4,                      3)
+   TAGKEYS(                        XK_5,                      4)
+   TAGKEYS(                        XK_6,                      5)
+   TAGKEYS(                        XK_7,                      6)
+   TAGKEYS(                        XK_8,                      7)
+   TAGKEYS(                        XK_9,                      8)
+
+    // Volume control
+    &((Keychord){1, {{MODKEY, XK_KP_Add}}   , spawn,          SHCMD(vol_up) }),
+    &((Keychord){1, {{MODKEY, XK_KP_Subtract}}   , spawn,          SHCMD(vol_down) }),
+    &((Keychord){1, {{MODKEY, XK_KP_Multiply}}   , spawn,          SHCMD(vol_mute) }),
 
 };
 
