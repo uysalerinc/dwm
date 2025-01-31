83534042600477efbe6464967ad7fae3d3253dc4
Erinç Uysal <erincuysal@outlook.com>
2025-02-01 01:55:53 +0300

    update dmenukey

--- config.def.h
+++ config.def.h
@@ -64,7 +64,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
