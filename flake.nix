b9436897a86052e2bc8bd925c37d79477601f64c
Erinç Uysal <erincuysal@outlook.com>
2025-02-24 00:06:49 +0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My custom DWM window manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+      name = "dwm";
+      src = self;
+
+      buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.xorg.libX11 ];
+
+      prePatch = ''
+        sed -i 's@/usr/local@$out@g' config.mk
+      '';
+
+      installPhase = ''
+        make PREFIX=$out install
+      '';
+    };
+  };
+}
